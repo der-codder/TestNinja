22ee55fa264f7633ba82cb4cf300bcd13f548d0f
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2018-07-31 16:57:42 +0300

    8. Testing All the Execution Tests

--- TestNinja.UnitTests/ReservationTests.cs
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -19,5 +19,26 @@ namespace TestNinja.UnitTests
             // Assert
             Assert.IsTrue(result);
         }
+
+        [TestMethod]
+        public void CanBeCancelledBy_SameUserCancellingTheReservation_ReturnTrue()
+        {
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+
+            var result = reservation.CanBeCancelledBy(user);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_AnotherUserCancellingReservation_ReturnFalse()
+        {
+            var reservation = new Reservation { MadeBy = new User() };
+
+            var result = reservation.CanBeCancelledBy(new User());
+
+            Assert.IsFalse(result);
+        }
     }
 }
